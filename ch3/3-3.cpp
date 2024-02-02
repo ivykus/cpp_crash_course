20e7408ed7a4a958c055b9ad5979276537d81315
vkudr <themoonissilent@gmail.com>
2024-02-02 18:16:00 +0300

    add 3-3 3-4

---
+++ ch3/3-3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+struct Element {
+  Element *next{};
+  Element *prev{};
+  void insert_after(Element *new_element) {
+    if (next) {
+      next->prev = new_element;
+    }
+    new_element->prev = this;
+
+    new_element->next = next;
+    next = new_element;
+  }
+  void insert_before(Element *new_element) {
+      if (prev) {
+          prev->next = new_element;
+      }
+      new_element->next = this;
+      new_element->prev = prev;
+      prev = new_element;
+  }
+  char prefix[2];
+  short operating_number;
+};
+
+int main() {
+  Element trooper1, trooper2, trooper3, trooper4;
+  trooper1.prefix[0] = 'T';
+  trooper1.prefix[1] = 'K';
+  trooper1.operating_number = 421;
+  trooper1.insert_after(&trooper2);
+  trooper2.prefix[0] = 'F';
+  trooper2.prefix[1] = 'N';
+  trooper2.operating_number = 2187;
+  trooper2.insert_after(&trooper3);
+  trooper3.prefix[0] = 'L';
+  trooper3.prefix[1] = 'S';
+  trooper3.operating_number = 005;
+  trooper3.insert_before(&trooper4);
+  trooper4.prefix[0] = 'L';
+  trooper4.prefix[1] = 'U';
+  trooper4.operating_number = 666;
+
+  printf("Traverse fron to back:\n");
+
+  for (Element *cursor = &trooper1; cursor; cursor = cursor->next) {
+    printf("stormtrooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1],
+           cursor->operating_number);
+  }
+
+  printf("Traverse back to front:\n");
+  for (Element *cursor = &trooper3; cursor; cursor = cursor->prev) {
+    printf("stormtrooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1],
+           cursor->operating_number);
+  }
+}
